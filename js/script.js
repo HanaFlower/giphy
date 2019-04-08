90cf783aa522bc224ca74f139ab2bb2f84eb9e93
Daisy Huerta <daisyhuerta221d.h@gmail.com>
2019-04-08 20:59:03 +0000

    code is woking! yay!

--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
-});
+  var userInput = $("#search-term").val();
 
+    var requesturl = "https://api.giphy.com/v1/gifs/search?q=" + userInput +"&rating=pg&api_key=dc6zaTOxFJmzC";
+    
+    $.ajax({
+      url: requesturl,
+      method: "GET",
+      success: function(response) {
+           var pic_url = response.data[0].images.original.url;
+           $('.gallery').append('<img src=' + pic_url + '>');
+      },
+    }); 
+  
+  
+
+});
